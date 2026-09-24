628726ceb9c259b5948628e25ed5852977eab554
Derek G <56454984+DerekGPrgm@users.noreply.github.com>
2026-09-24 19:13:15 -0400

    Added lesson 1 final project

---
+++ Part 1 - Syntax, Variables, If-Else/final_project/7_ifelse.py
@@ -0,0 +1,19 @@
+first  = 3
+second = 5
+third  = 4
+fourth = 6
+fifth  = 2
+
+## Don't modify above this line ##
+
+def testNumber(num):
+    if num > 10:
+      print(num)
+
+## Don't modify below this line ##
+
+testNumber(first)
+testNumber(second)
+testNumber(third)
+testNumber(fourth)
+testNumber(fifth)
